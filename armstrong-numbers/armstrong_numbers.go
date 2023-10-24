47d546188c6dae90e9f57cff8e4ca4d9fc7a54b7
Lothar Schulz <lothar.sch@gmx.de>
2023-10-24 13:03:26 +0200

    chore func names

--- armstrong-numbers/armstrong_numbers.go
+++ armstrong-numbers/armstrong_numbers.go
@@ -10,8 +10,8 @@ func IsNumber(n int) bool {
 	if n < 0 {
         return false
     }
-	digits := getDigits(n)
-    numberOfDigits := getNumberOfDigits(digits)
+	digits := GetDigits(n)
+    numberOfDigits := GetNumberOfDigits(digits)
     total := 0
 	for _, v := range digits{
         total = total + int(math.Pow(float64(v),float64(numberOfDigits)))
@@ -24,24 +24,24 @@ func IsNumber(n int) bool {
 }
 
 // returns the digits with a given integer
-func getDigits(n int) []int{
+func GetDigits(n int) []int{
     digits := []int{}
     for n !=0 {
         digits = append(digits, n % 10)
         n /= 10
     }
-    reverseInt(digits)
+    ReverseInt(digits)
     return digits
 }
 
 // reverses a given int array
-func reverseInt(s []int) {
+func ReverseInt(s []int) {
     for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
             s[i], s[j] = s[j], s[i]
     }
 }
 
 // returns the number of digits/integers of an int array
-func getNumberOfDigits(digits []int) int{
+func GetNumberOfDigits(digits []int) int{
 	return len(digits)
 }
