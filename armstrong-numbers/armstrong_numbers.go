640fc9e20c423ee5db4c234c3b6cc7b37dbc444b
Lothar Schulz <lothar.sch@gmx.de>
2023-10-24 12:48:42 +0200

    chore armstrong numbers tests pass

--- armstrong-numbers/armstrong_numbers.go
+++ armstrong-numbers/armstrong_numbers.go
@@ -1,5 +1,44 @@
 package armstrong
 
+import (
+    "math"
+)
+
+// determines if a given number is an Armstrong number
+// see also https://github.com/lotharschulz/armstrongNumbers/
 func IsNumber(n int) bool {
-	panic("Please implement the IsNumber function")
+	if n < 0 {
+        return false
+    }
+	digits := getDigits(n)
+    numberOfDigits := getNumberOfDigits(digits)
+    total := 0
+	for _, v := range digits{
+        total = total + int(math.Pow(float64(v),float64(numberOfDigits)))
+    }
+    if total == n{
+        return true
+    }
+    return false
+
+}
+
+func getDigits(n int) []int{
+    digits := []int{}
+    for n !=0 {
+        digits = append(digits, n % 10)
+        n /= 10
+    }
+    reverseInt(digits)
+    return digits
+}
+
+func reverseInt(s []int) {
+    for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+            s[i], s[j] = s[j], s[i]
+    }
+}
+
+func getNumberOfDigits(digits []int) int{
+	return len(digits)
 }
