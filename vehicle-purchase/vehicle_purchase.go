b13f7502391c56bad4b667fe649b9f5898b3b890
Lothar Schulz <lothar.sch@gmx.de>
2023-11-02 11:29:04 +0100

    chore func ChooseVehicle implemented

--- vehicle-purchase/vehicle_purchase.go
+++ vehicle-purchase/vehicle_purchase.go
@@ -10,7 +10,10 @@ func NeedsLicense(kind string) bool {
 
 // ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
 func ChooseVehicle(option1, option2 string) string {
-	panic("ChooseVehicle not implemented")
+	if option1 < option2 {
+		return option1 + " is clearly the better choice."
+	}
+	return option2 + " is clearly the better choice."
 }
 
 // CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
